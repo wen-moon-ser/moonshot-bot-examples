8aadeb0c7ccfab5fc3c95e4f61048770f740c675
martinavagyan <martinavagyan@gmail.com>
2024-07-02 11:19:45 +0300

    feat: fix the package version

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
-export const greet = (name: string): string => {
-  return `Hello, ${name}!`;
+import { Moonshot, Environment } from '@wen-moon-ser/moonshot-sdk';
+
+const main = async (): Promise<void> => {
+  const rpcUrl = 'https://api.mainnet-beta.solana.com';
+
+  const moonshot = new Moonshot({
+    rpcUrl,
+    authToken: 'YOUR_AUTH_TOKEN',
+    environment: Environment.MAINNET,
+  });
+
+  const token = moonshot.Token({
+    mintAddress: 'AhaAKM3dUKAeYoZCTXF8fqqbjcvugbgEmst6557jkZ9h',
+  });
+
+  const curvePos = await token.getCurvePosition();
+  console.log(curvePos);
 };
 
-greet('World');
+main().catch(console.error);
